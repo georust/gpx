a8dda82525a7411f09f9e6719c54ffeadf470027
Peter Kirk <peter.kirk@student.uibk.ac.at>
2018-03-20 14:32:25 +0100

    Ran cargo fmt for cleaner formatting

--- src/parser/extensions.rs
+++ src/parser/extensions.rs
@@ -10,29 +10,40 @@ use parser::Context;
 
 /// consume consumes the entire content of the extension tag and returns as string
 pub fn consume<R: Read>(context: &mut Context<R>) -> Result<(String)> {
-    let mut extension_xml : Option<String> = None;
+    let mut extension_xml: Option<String> = None;
     let mut depth = 1; //this consume function is called when the opening extension tag was already read (except in tests)
     for event in context.reader() {
         match event.chain_err(|| "error while parsing XML")? {
             XmlEvent::StartElement { name, .. } => {
-                if &name.local_name == "extensions" && depth == 1 { //needed because tests have this tag in, actual parsing has it stripped already
+                if &name.local_name == "extensions" && depth == 1 {
+                    //needed because tests have this tag in, actual parsing has it stripped already
                     continue;
                 }
-                ensure!(&name.local_name != "extensions", "extensions tag opened twice");
-                extension_xml = Some(extension_xml.unwrap_or("".to_string()) + "<" + &name.borrow().to_repr() + ">");
+                ensure!(
+                    &name.local_name != "extensions",
+                    "extensions tag opened twice"
+                );
+                extension_xml = Some(
+                    extension_xml.unwrap_or("".to_string()) + "<" + &name.borrow().to_repr() + ">",
+                );
                 depth += 1;
             }
 
-            XmlEvent::Characters(content) => extension_xml = Some(extension_xml.unwrap_or("".to_string()) + &content),
+            XmlEvent::Characters(content) => {
+                extension_xml = Some(extension_xml.unwrap_or("".to_string()) + &content)
+            }
 
             XmlEvent::EndElement { name, .. } => {
                 depth -= 1;
                 ensure!(depth >= 0, "extensions tag contained invalid xml");
-                if &name.local_name == "extensions"  {
+                if &name.local_name == "extensions" {
                     ensure!(depth == 0, "extensions tag contained invalid xml");
                     return extension_xml.ok_or("no content inside extensions".into());
                 } else {
-                    extension_xml = Some(extension_xml.unwrap_or("".to_string()) + "</" +  &name.borrow().to_repr() + ">");
+                    extension_xml = Some(
+                        extension_xml.unwrap_or("".to_string()) + "</" + &name.borrow().to_repr()
+                            + ">",
+                    );
                 }
             }
 
